b9482a30838ebb4bc1e3fa09bf388d31756a6917
Kselvaraj <Kselvaraj@WBLT26W8H72.wbhq.com>
2018-01-15 22:56:45 -0600

    jsad

---
+++ src/test/java/dotcom/APITest.java
@@ -0,0 +1,27 @@
+package dotcom;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class APITest {
+
+
+		
+		@Test
+		public void accountservices() {
+			
+			System.out.println("Savings");
+			
+			WebDriver driver;
+		}
+		
+		
+		@Test
+		public void Checkingaccount() {
+			
+			System.out.println("Checkings");
+			
+		}
+
+
+}
---
+++ src/test/java/dotcom/appiumupdatedTest.java
@@ -0,0 +1,23 @@
+package dotcom;
+
+import org.testng.annotations.Test;
+
+public class appiumupdatedTest {
+	
+
+		
+		@Test
+		public void android() {
+			
+			System.out.println("Android");
+		}
+		
+		
+		@Test
+		public void IOS() {
+			
+			System.out.println("IOS");
+			
+		}
+
+	}
